8a917d567fe8127fb18151839068301fd2769ad6
senujaBodhinayake <senujabodhinayake123@gmail.com>
2025-08-16 00:21:59 +0530

    menu page done

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router'
 import { Toaster } from 'react-hot-toast'
+import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </CartProvider>
   </StrictMode>
 );
